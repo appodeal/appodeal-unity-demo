35be68c9d339078709ced203e0fe453232013261
KulArtsiom <kulartsiom@gmail.com>
2019-08-22 12:40:52 +0300

    add samples

--- Assets/Appodeal/Common/IPermissionGrantedListener.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AppodealAds.Unity.Common {
-	public interface IPermissionGrantedListener {
-		void writeExternalStorageResponse(int result);
-		void accessCoarseLocationResponse(int result);
-	}
-}
